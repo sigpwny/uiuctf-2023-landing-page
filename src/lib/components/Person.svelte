160a08d6e6fa995275f7e97279175986fb0c7d16
Peter Stenger <peter.a.stenger@gmail.com>
2023-05-21 00:11:39 -0400

    finish template

---
+++ src/lib/components/Person.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name = '';
+    export let img = '';
+    export let href = '';
+</script>
+
+<div class="col-6 col-md-4 col-lg-3 d-flex align-items-center flex-column mb-2">
+    <img src={img} class="img-fluid" alt="">
+    <a {href}>
+        <span class="font-xsmall mt-1">{name}</span>
+    </a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 
 </svelte:head>
 <script>
+    import { draggable } from '@neodrag/svelte'
+    import Person from '../lib/components/Person.svelte'
     let time = new Date().toLocaleTimeString('en-US', { 
         hour: "numeric", 
         minute: "numeric"
@@ -10,103 +12,136 @@
     $: selected = "about"
 </script>
 <style>
-    .bg-light-yellow {
+    .info-panel {
         background: #feffd1;
+        max-height: 300px;
+        overflow: auto;
     }
 </style>
 <div class="min-vh-100 d-flex flex-column">
-
-<main class="d-flex flex-grow-1 w-100">
-     <!-- Section -->
-     <section class="d-flex align-items-center bg-secondary w-100">
-        <div class="container">
-            <div class="col-12 col-lg-6 mb-4 mb-lg-0">
-                <div class="card card-tertiary">
-                    <div class="card-header d-flex flex-row justify-content-between">
-                        <span>Welcome</span>
-                        <span class="btn btn-primary px-1 pt-0 pb-0">
-                            <img src="images/windows-95-close-icon.png" alt="">
-                        </span>
-                    </div>
-                    <div class="card-body">
-                        <h4>Welcome to UIUCTF 2023</h4>
-                        <div class="d-flex mt-3 flex-column">
-                            <div class="d-flex flex-row">
-                                
-                                <div class="bg-light-yellow d-flex flex-column p-3 overflow-scroll w-100">
-                                    {#if selected == "about"}
-                                        <div class="d-flex flex-row align-items-center">
-                                            <img src="images/windows-95-tip.png" alt="">
-                                            <p class="font-weight-bold ml-2 mt-2">Did you know...</p>
-                                        </div>
-                                        <p class="d-flex flex-row">
-                                            You can use the buttons on the right to learn more about UIUCTF! You can also head right to the CTF.
-                                        </p>
-                                    {:else if selected == "when"}
-                                        <p>
-                                        UIUCTF 2023 will be held Sat, 01 July 2023, 00:00 UTC — Mon, 03 July 2023, 00:00 UTC.
-                                        </p>
-                                    {:else if selected == "prizes"}
-                                        <p>
-                                            Prizes TBD.
-                                        </p>
-                                    {:else if selected == "sponsors"}
-                                        <p>
-                                            SIGPwny thanks its UIUCTF 2023 sponsors:
-                                        </p>
-                                        <img src="images/trail-of-bits.png" class="img-fluid" alt="">
-                                        <img src="images/google-cloud.png" class="img-fluid" alt="">
-                                    {:else if selected == "team"}
-                                        <p>Meet the UIUCTF 2023 Team</p>
-                                    {/if}
-                                        
-                                </div>
-                            <div class="d-flex flex-column pl-2">
-                                <button class={`btn btn-sm mb-1 btn-primary ${selected == 'about' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'about'}}><span
-                                    class="btn-text">About</span></button>
-                                <button class={`btn btn-sm mb-1 btn-primary ${selected == 'when' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'when'}}><span
-                                    class="btn-text">When</span></button>
-                                    <button class={`btn btn-sm mb-1 btn-primary ${selected == 'prizes' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'prizes'}}><span
-                                        class="btn-text">Prizes</span></button>
-                                <button class={`btn btn-sm mb-1 btn-primary ${selected == 'sponsors' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'sponsors'}}><span
-                                    class="btn-text">Sponsors</span></button>
-                                    <button class={`btn btn-sm mb-1 btn-primary ${selected == 'team' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'team'}}><span
-                                        class="btn-text">Team</span></button>
-                                    </div>
-                                </div>
-                            <div class="d-flex flex-row">
-                                
-                                <div class="form-check">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="checkbox">
-                                    <span class="form-check-x"></span>
-                                    <span class="form-check-sign"></span>
-                                    <span class="font-xsmall">
-                                        Show this Welcome Screen next time you start Windows
-                                    </span>
-                                </label>
-                            </div>
+    <main class="d-flex flex-grow-1 w-100">
+        <!-- Home screen -->
+        <section class="d-flex bg-secondary w-100">
+            <div class="d-flex flex-column mt-3 ml-3 position-fixed">
+                <div class="d-flex flex-column">
+                    <img src="images/windows-95-my-computer.png" width="32" height="32" alt="">
+                    <span class="font-xsmall text-neutral mt-2 mb-4">My Computer</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <img src="images/windows-95-network-neighborhood.png" width="32" height="32" alt="">
+                    <span class="font-xsmall text-neutral mt-2 mb-4">Network <br> Neighborhood</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <img src="images/windows-95-inbox.png" width="32" height="32" alt="">
+                    <span class="font-xsmall text-neutral mt-2 mb-4">Inbox</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <img src="images/windows-95-recycle-bin.png" width="32" height="32" alt="">
+                    <span class="font-xsmall text-neutral mt-2 mb-4">Recycle Bin</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <img src="images/windows-95-msn.png" width="32" height="32" alt="">
+                    <span class="font-xsmall text-neutral mt-2 mb-4">Set up the <br> SIGPwny <br> Network</span>
+                </div>
+            </div>
+            <div class="d-flex container align-self-center justify-content-center">
+                <div class="col-12 col-md-6 col-lg-8 mb-4 mb-lg-0">
+                    <div class="card card-tertiary" use:draggable >
+                        <div class="card-header d-flex flex-row justify-content-between">
+                            <span>Welcome</span>
+                            <span class="btn btn-primary px-1 pt-0 pb-0">
+                                <img src="images/windows-95-close-icon.png" alt="">
+                            </span>
                         </div>
+                        <div class="card-body">
+                            <h5>Welcome to <b>UIUCTF 2023</b></h5>
+                            <div class="d-flex mt-3 flex-column">
+                                <div class="d-flex flex-row">
+                                    
+                                    <div class="info-panel d-flex flex-column p-3 w-100">
+                                        {#if selected == "about"}
+                                            <div class="d-flex flex-row align-items-center">
+                                                <img src="images/windows-95-tip.png" alt="">
+                                                <p class="font-weight-bold ml-2 mt-2">Did you know...</p>
+                                            </div>
+                                            <p class="d-flex flex-row">
+                                                You can use the buttons on the right to learn more about UIUCTF! You can also head right to the CTF.
+                                            </p>
+                                        {:else if selected == "when"}
+                                            <p>
+                                            UIUCTF 2023 will be held Sat, 01 July 2023, 00:00 UTC — Mon, 03 July 2023, 00:00 UTC.
+                                            </p>
+                                        {:else if selected == "prizes"}
+                                            <p>
+                                                Prizes TBD.
+                                            </p>
+                                        {:else if selected == "sponsors"}
+                                            <p>
+                                                SIGPwny thanks its UIUCTF 2023 sponsors:
+                                            </p>
+                                            <div class="row">
+                                                <div class="col-8 mb-2">
+                                                    <img src="images/google-cloud.png" class="img-fluid" alt="">
+                                                </div>
+                                                <div class="col-8">
+                                                    <img src="images/trail-of-bits.png" class="img-fluid" alt="">
+                                                </div>
+                                            </div>
+                                        {:else if selected == "team"}
+                                            <div class="d-flex flex-column">
+                                                <p>Meet the UIUCTF 2023 Team</p>
+                                                <div class="row">
+                                                    <Person name="Pete Stenger" img="https://avatars.githubusercontent.com/u/13869303" href="https://github.com/reteps" />
+                                                </div>
+                                            </div>
+                                        {/if}
+                                            
+                                    </div>
+                                <div class="d-flex flex-column pl-2">
+                                    <button class={`btn btn-sm mb-1 btn-primary ${selected == 'about' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'about'}}><span
+                                        class="btn-text">About</span></button>
+                                    <button class={`btn btn-sm mb-1 btn-primary ${selected == 'when' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'when'}}><span
+                                        class="btn-text">When</span></button>
+                                        <button class={`btn btn-sm mb-1 btn-primary ${selected == 'prizes' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'prizes'}}><span
+                                            class="btn-text">Prizes</span></button>
+                                    <button class={`btn btn-sm mb-1 btn-primary ${selected == 'sponsors' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'sponsors'}}><span
+                                        class="btn-text">Sponsors</span></button>
+                                        <button class={`btn btn-sm mb-1 btn-primary ${selected == 'team' ? 'border-dark' : ''}`} type="button" on:click={() => {selected = 'team'}}><span
+                                            class="btn-text">Team</span></button>
+                                        </div>
+                                    </div>
+                                <div class="d-flex flex-row mt-2">
+                                    <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox">
+                                        <span class="form-check-x"></span>
+                                        <span class="form-check-sign"></span>
+                                        <span class="font-xsmall">
+                                            Show this Welcome Screen next time you start Windows
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
-<footer class="d-flex">
-    <nav id="navbar-footer" class="navbar navbar-main navbar-expand-lg navbar-dark justify-content-between navbar-footer">
-        <ul class="navbar-nav navbar-nav-hover flex-row align-items-center">
-            <li class="nav-item">
-                <div class="nav-link">
-                    <img src="/images/windows-95-start.png" alt="" style="opacity: 100% !important">
-                </div>
-            </li>
-        </ul>
-        <div class="time text-center">
-            <img src="/images/windows-95-speaker.png" alt="">
-            {time}
-        </div>
-    </nav>
-</footer>
+        </section>
+    </main>
+    <footer class="d-flex">
+        <nav id="navbar-footer" class="navbar navbar-main navbar-expand-lg navbar-dark justify-content-between navbar-footer">
+            <ul class="navbar-nav navbar-nav-hover flex-row align-items-center">
+                <li class="nav-item">
+                    <div class="nav-link">
+                        <img src="/images/windows-95-start.png" alt="" style="opacity: 100% !important; padding-bottom: 2px;">
+                    </div>
+                </li>
+            </ul>
+            <div class="time text-center">
+                <img src="/images/windows-95-speaker.png" alt="">
+                {time}
+            </div>
+        </nav>
+    </footer>
 </div>
