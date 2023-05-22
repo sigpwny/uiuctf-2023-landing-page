769b5a4e4dd5b084cda084598f9257a53174d6ba
Pete Stenger <peter.a.stenger@gmail.com>
2023-05-21 19:01:57 -0500

    Add kuilin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -99,6 +99,7 @@
                                                     <Person name="Pete Stenger" img="https://avatars.githubusercontent.com/u/13869303" href="https://github.com/reteps" />
                                                     <Person name="Richard Liu" img="https://avatars.githubusercontent.com/u/9328196" href="https://github.com/richyliu" />
                                                     <Person name="YiFei Zhu" img="https://lh3.googleusercontent.com/a-/ACB-R5SiFvHe6qP5yi3wJVNMUQqolSc-SRIl_ncQvhC_" href="https://github.com/zhuyifei1999" />
+                                                    <Person name="kuilin" img="https://kuilin.net/profile_upscaled.png" href="https://kuilin.net/" />
                                                 </div>
                                             </div>
                                         {/if}
